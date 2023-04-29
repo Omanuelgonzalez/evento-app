d6d12ec19882666f5372e4e6d4072c0650c75f39
unknown <est.Ogonxalezx0201@uml.edu.ni>
2023-04-29 08:06:43 -0600

    subiendo

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import '../css/style.css';
+
+//Iniciar todos los componentes dinamicos de flowbite
+import 'flowbite';
+//Barra de la pagina
+import './paginas/barra.js';
+//importar header
+import './header';
+//importar carrousel
+import './carrousel';
